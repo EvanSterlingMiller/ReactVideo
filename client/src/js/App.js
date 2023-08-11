5798086b36b8ed1c8d0ea159ac3bc662c8b4114e
EvanSterlingMiller <evan.s.miller3@gmail.com>
2023-08-11 14:25:05 -0400

    app

---
+++ client/src/js/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { socket, PeerConnection } from './communication'
+import _ from 'lodash';
+import MainWindow from './components/MainWindow';
+import CallWindow from './components/CallWindow';
+import CallModal from './components/CallModel';
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            
+        }
+    }
+}
