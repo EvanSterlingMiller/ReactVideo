2a81ed147023c4004aac0c0be42f024761a993e5
EvanSterlingMiller <evan.s.miller3@gmail.com>
2023-08-11 14:14:55 -0400

    client

---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import app from './js/App'
+import './css/app/scss'
+
+const root = createRoot(document.getElementById('root'))
+root.render(<APP />)
